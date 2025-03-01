ca332605485d2bc5af88510234920f315039b153
vonhuhoanghuy <vonhuhoanghuy@gmail.com>
2025-03-01 13:53:30 +0700

    fix

--- src/router.js
+++ src/router.js
@@ -28,22 +28,19 @@ const RouterCustom = () => {
         <Route path={ROUTERS.USERS.CART} element={<CartPage />} />
         <Route path={ROUTERS.USERS.LOOKUP} element={<LookupPage />} />
         <Route path={ROUTERS.USERS.VERIFY} element={<VerifyOTP />} />
-        
       </Route>
       <Route element={<AdminLayout />}>
         <Route path={ROUTERS.ADMIN.DASHBOARD} element={<Dashboard />} />
-        <Route
-          path={ROUTERS.ADMIN.CREATE_PRODUCT}
-          element={<CreateProduct />}
-        />
+        <Route path={ROUTERS.ADMIN.CREATE_PRODUCT} element={<CreateProduct />} />
       </Route>
       <Route element={<ProfilePageLayout />}>
         <Route path={ROUTERS.USERS.PROFILE_PAGE} element={<ProfilePage />} />
-        <Route path={ROUTERS.USERS.ORDER_MANAGEMENT} element={< OrderManagement/>} / >
-        <Route path={ROUTERS.USERS.VIEW_PRODUCT} element={< ViewHistories/>} / >
-        <Route path={ROUTERS.USERS.ADRESS_BOOK} element={< AddressBook/>} />
+        <Route path={ROUTERS.USERS.ORDER_MANAGEMENT} element={<OrderManagement />} />
+        <Route path={ROUTERS.USERS.VIEW_PRODUCT} element={<ViewHistories />} />
+        <Route path={ROUTERS.USERS.ADRESS_BOOK} element={<AddressBook />} />
       </Route>
     </Routes>
   );
 };
+
 export default memo(RouterCustom);
