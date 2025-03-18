bbab24f79aa19b2672f77de184a266e3680ef9fe
vonhuhoanghuy <vonhuhoanghuy@gmail.com>
2025-03-18 21:22:37 +0700

    addressbook

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const apiLink = "http://localhost:3001";
-export const apiLink = "http://10.50.0.61:3001";
+export const apiLink = "http://localhost:3001";
+// export const apiLink = "http://10.50.0.61:3001";
